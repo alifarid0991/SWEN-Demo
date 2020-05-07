048fb99fcb1401e1fa470c80241ff6acc5bc957c
alifarid0991 <baccasdboy@gmail.com>
2020-05-07 16:02:06 +0300

    demo prepaartion

--- swen-video-indexer/swen-video-indexer/swen-backend/srv/swen/apps/video/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('upload'    , views.upload_video  , name='upload_video'),
-    path('info'      , views.get_video_info      , name='get_video_info'),
-    path('search'    , views.seach_video   , name='search_in_video_info'),
-]
